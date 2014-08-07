729aa90ec027228a545c8a0569fbb49bf034ecb2
Tristan Harris <tris.dev@pumpcottage.me.uk>
2014-08-07 20:29:26 +0100

    Build different card types

--- lib/credit_card.rb
+++ lib/credit_card.rb
@@ -1,7 +1,24 @@
 module CreditCard
 
   def self.build(card_number)
-    Visa.new
+    if %w(34 37).include? card_number[0..1]
+      AmEx.new
+    elsif card_number.start_with? '6011'
+      Discover.new
+    elsif ('51'..'55').include? card_number[0..1]
+      MasterCard.new
+    elsif card_number.start_with? '4'
+      Visa.new
+    end
+  end
+
+  class AmEx
+  end
+
+  class Discover
+  end
+
+  class MasterCard
   end
 
   class Visa
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -2,10 +2,37 @@ require_relative '../lib/credit_card'
 
 describe CreditCard, 'build' do
 
-  context 'with a visa number' do
+  context 'with an AmEx number' do
+
+    it 'creates an AmEx card' do
+      cc = CreditCard.build '34'
+      expect(cc).to be_a CreditCard::AmEx
+    end
+
+  end
+
+  context 'with a Discover number' do
+
+    it 'creates a Discover card' do
+      cc = CreditCard.build '6011'
+      expect(cc).to be_a CreditCard::Discover
+    end
+
+  end
+
+  context 'with a MasterCard number' do
+
+    it 'creates a MasterCard' do
+      cc = CreditCard.build '51'
+      expect(cc).to be_a CreditCard::MasterCard
+    end
+
+  end
+
+  context 'with a Visa number' do
 
     it 'creates a Visa card' do
-      cc = CreditCard.build('4')
+      cc = CreditCard.build '4'
       expect(cc).to be_a CreditCard::Visa
     end
 
